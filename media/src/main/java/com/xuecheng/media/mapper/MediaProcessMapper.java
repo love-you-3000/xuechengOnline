52c4638d41ea503f20ac4af590ca71976c58bd60
朱江 <xiaozhu09110099@163.com>
2023-06-11 19:07:02 +0800

    使用mysql实现分布式锁

--- media/src/main/java/com/xuecheng/media/mapper/MediaProcessMapper.java
+++ media/src/main/java/com/xuecheng/media/mapper/MediaProcessMapper.java
@@ -4,6 +4,7 @@ package com.xuecheng.media.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xuecheng.media.entity.MediaProcess;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -17,4 +18,14 @@ import java.util.List;
 public interface MediaProcessMapper extends BaseMapper<MediaProcess> {
     @Select("SELECT * FROM media_process WHERE id%#{shardTotal}=#{shardIndex} AND (`status`=1 OR `status`=3) AND fail_count<3 LIMIT #{count}")
     List<MediaProcess> selectByShardIndex(int shardTotal, int shardIndex, int count);
+
+    /**
+     * 开启一个任务
+     *
+     * @param id 任务id
+     * @return 更新记录数
+     */
+    @Update("UPDATE media_process m SET m.status='4' WHERE (m.status='1' OR m.status='3') AND m.fail_count<3 AND m.id=#{id}")
+    int startTask(long id);
+
 }
