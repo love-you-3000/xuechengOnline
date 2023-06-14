f477537599e98ff90a91adfaaa0f0e557ff17eb2
朱江 <xiaozhu09110099@163.com>
2023-06-14 16:26:11 +0800

    引入消息处理sdk

---
+++ message-sdk/src/main/java/com/xuecheng/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.xuecheng.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.messagesdk.entity.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
