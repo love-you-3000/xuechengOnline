ad5eacb28ecba111f60c6f322c98a21914416689
朱江 <xiaozhu09110099@163.com>
2023-06-21 12:52:22 +0800

    添加选课模块

---
+++ learning/src/main/java/com/xuecheng/learning/mapper/XcChooseCourseMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.learning.entity.XcChooseCourse;
+
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcChooseCourseMapper extends BaseMapper<XcChooseCourse> {
+
+}
---
+++ learning/src/main/java/com/xuecheng/learning/mapper/XcCourseTablesMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xuecheng.learning.entity.XcCourseTables;
+
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCourseTablesMapper extends BaseMapper<XcCourseTables> {
+
+}
